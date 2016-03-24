5371fd77095ba2dd2e19fa3ba484602fa957bbe3
Teng <843018739@qq.com>
2016-03-24 13:42:57 +0800

    init project

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('madAdvertiserWeb', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();

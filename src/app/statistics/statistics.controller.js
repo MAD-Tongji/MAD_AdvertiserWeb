e6a3e61eb3d0a4f54d5e6993eb2edf51f3f8ce70
MandyXue <13564039693@163.com>
2016-03-31 14:51:17 +0800

    修改统计 TODO:广告统计的图还有些问题

--- src/app/statistics/statistics.controller.js
+++ src/app/statistics/statistics.controller.js
@@ -8,8 +8,9 @@
     .controller('AdvertSatisticsCtrl', AdvertSatisticsCtrl);
 
   function AdvertSatisticsCtrl($scope) {
-
-
+    $scope.totalPay = "111111";
+    $scope.totalTopUp = "222";
+    $scope.tableContent = [["123321245657","广告标题1","2016-01-01","100000","1000"],["123321245657","广告标题2","2016-02-01","200000","1500"]]
   }
 
 

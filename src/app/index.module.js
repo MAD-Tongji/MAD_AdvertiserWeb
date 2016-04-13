08c8453d3a822eb66fb273f88efb5bd10a14ce95
jimxiang <jxhzr94151@gmail.com>
2016-04-13 20:45:42 +0800

    delete unrelated files

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('mad', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
-
-})();
